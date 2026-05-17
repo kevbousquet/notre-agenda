19e38246930d297c58e6c87e724138d6782df79e
kevbousquet <kevbousquet@hotmail.fr>
2026-05-17 09:07:50 +0200

    chore: bump SW cache to v7 for members+deadline release

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'agenda-couple-v6';
+const CACHE = 'agenda-couple-v7';
 const BASE = '/notre-agenda';
 const ASSETS = [BASE+'/index.html', BASE+'/manifest.json', BASE+'/icons/icon-192.png', BASE+'/icons/icon-512.png'];
 
